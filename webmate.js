c420b04dbe699997bd6978592e1e932ba90dd65d
Robert Lowe <rlowe@semo.edu>
2022-09-18 07:20:17 -0500

    added license header to javascript file

--- webmate.js
+++ webmate.js
@@ -1,3 +1,21 @@
+/*
+    Webmate adds a little friend to your web pages.
+    Copyright (C) 2022 Robert Lowe
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 function webmate_load(dir) {
     if(dir[dir.length - 1] != '/') {
         dir = dir + '/'
@@ -66,6 +84,7 @@ function webmate_load(dir) {
         e.preventDefault();
         var preview = this.elem.cloneNode(true);
         preview.style.display = "none";
+        document.body.appendChild(preview);
         e.dataTransfer.setDragImage(preview, 0, 0);
         this.move(e.clientX, e.clientY);
     }
